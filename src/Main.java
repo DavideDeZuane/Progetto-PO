53a149acb6682806878bbebe74e6bb89e272d599
DavideDeZuane <s1092669@studenti.univpm.it>
2020-12-13 09:54:56 +0100

    todo list e alcune nuove feature

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,13 @@ import Controller.Lavoro;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         String url = "";
         ApiController controller = null;
+        HashSet<Lavoro> offerte = new HashSet<>();
         try {
             Scanner input = new Scanner(System.in);
 
@@ -19,17 +18,20 @@ public class Main {
                  System.out.println("Inserisci l'url");
                  url = input.nextLine();
                  controller = new ApiController(url);
-
             }while(!controller.isValidUrl(url));
-        }catch(MalformedURLException e){
+            /*offerte = (ArrayList<Lavoro>) controller.parsing();
+            for(Lavoro l: offerte) {
+                System.out.println(l);
+            }
+            */
+            offerte.addAll((HashSet)controller.parsing());
+            for(Lavoro l: offerte)
+                System.out.println(l);
+        }catch(MalformedURLException e) {
             System.out.println("Url non valido");
             e.printStackTrace();
         }
-
-        try {
-             Lavoro offerta = controller.parsing(controller.getUrl());
-             System.out.println(offerta);
-        }catch(IOException e){
+        catch(IOException e){
             e.printStackTrace();
             System.out.println("Problema nella deserializzazione");
         }
