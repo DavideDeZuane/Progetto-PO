fa7ec90909537f7e434caf9b1780da318455c0d8
Daniele Benfatto <S1092454@studenti.univpm.it>
2020-12-15 20:21:37 +0100

    Implementato test di utilizzo della libreria ApiController sulla GUI

--- src/GUI/BootstrapPanel.java
+++ src/GUI/BootstrapPanel.java
@@ -1,18 +1,27 @@
 package GUI;
 
+import Controller.ApiController;
+import Controller.PARAMETERES;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
+import java.util.EnumSet;
 
 public class BootstrapPanel extends JFrame{
 
     private JPanel rootPanel;
-    private JTextField txtType;
     private JTextField txtLocation;
-    private JTextField txtCompany;
     private JTextField txtDescription;
     private JButton btnSearch;
     private JButton btnShowJobsSaved;
+    private JCheckBox FullTime;
+    URL url = null;
+    private String filters[] = new String[100];
+
+    private ApiController apiController = null;
+
 
     public BootstrapPanel() {
 
@@ -26,6 +35,24 @@ public class BootstrapPanel extends JFrame{
         btnSearch.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                inserisciCampi();
+                apiController = new ApiController();
+
+/*
+                if(!txtDescription.equals("")){
+                    EnumSet<PARAMETERES> flags = EnumSet.of(PARAMETERES.LOCATION, PARAMETERES.DESCRIPTION);
+                }*/
+                EnumSet<PARAMETERES> flags = EnumSet.of(PARAMETERES.LOCATION, PARAMETERES.DESCRIPTION);
+
+                try {
+
+                    url = ApiController.query(filters, flags);
+
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+
                 new JobsFoundPanel();
             }
         });
@@ -36,5 +63,10 @@ public class BootstrapPanel extends JFrame{
                 new JobsSavedPanel();
             }
         });
+    }
+
+    public void inserisciCampi(){
+        filters[0] = txtDescription.getText();
+        filters[1] = txtLocation.getText();
     }
 }
