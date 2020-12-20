3b4f9d6e6df9a21c4eb47c867993498c7bef97a0
Chiara <chiaragobbi2001@gmail.com>
2020-12-20 23:43:36 +0100

    implemented barchart

--- src/GUI/StatsJobsFoundPanel.java
+++ src/GUI/StatsJobsFoundPanel.java
@@ -4,11 +4,19 @@ import Model.Job;
 import Model.StatsJobBoard;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.TickUnitSource;
 import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashSet;
@@ -23,8 +31,10 @@ public class StatsJobsFoundPanel extends JFrame {
     private JLabel lblKeyWordRepeat;
     private JButton showChartButton;
     private final StatsJobBoard statsJobBoard;
-    JFreeChart chart;
+    private JFreeChart chart;
     private DefaultPieDataset dataset;
+    private XYSeries series;
+    private XYSeriesCollection seriesCollection;
 
     public StatsJobsFoundPanel(HashSet<Job> jobs, String keyWord){
 
@@ -41,6 +51,31 @@ public class StatsJobsFoundPanel extends JFrame {
         XYPlot plot1 = (XYPlot) histogram.getPlot();
         Plot plot2 =  histogram.getPlot();*/
 
+        series = new XYSeries("Number of Jobs");
+        for(int i=0; i<30; i++)
+            series.add(i, statsJobBoard.dateOfCreationBis()[i]);
+        seriesCollection = new XYSeriesCollection();
+        seriesCollection.addSeries(series);
+
+        JFreeChart barchart = ChartFactory.createXYBarChart("Date of creation", "Days", false, "Jobs", seriesCollection, PlotOrientation.VERTICAL, true, true, false);
+        ChartFrame frame1 = new ChartFrame("Charts", barchart);
+        //frame1.pack();
+
+        XYPlot plot1= (XYPlot) barchart.getXYPlot();
+        TickUnitSource ticks = NumberAxis.createIntegerTickUnits();
+        plot1.getDomainAxis().setRange(0,30);
+        plot1.getDomainAxis().setStandardTickUnits(ticks);
+
+
+       // TickUnitSource unitSource = new TickUnits();
+
+       // plot1.getDomainAxis().setStandardTickUnits(unitSource);
+       // plot1.getRangeAxis().setStandardTickUnits(unitSource);
+        //ValueAxis numberAxis = plot1.getRangeAxis();
+
+
+        //ValueAxis domainAxis = plot1.getDomainAxis();
+        //domainAxis.setStandardTickUnits();
 
         //plot2.getDomainAxis().setAutoRange(true);
 
@@ -93,12 +128,12 @@ public class StatsJobsFoundPanel extends JFrame {
         showChartButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                /*JFrame fr = new JFrame("Chart");
-                fr.getContentPane().add(new ChartPanel(chart), BorderLayout.WEST);
-                fr.getContentPane().add(new ChartPanel(histogram), BorderLayout.EAST);
+                JFrame fr = new JFrame("Chart");
+                fr.getContentPane().add(new ChartPanel(chart), BorderLayout.EAST);
+                fr.getContentPane().add(new ChartPanel(barchart), BorderLayout.WEST);
                 fr.pack();
-                fr.setVisible(true);*/
-                frame.setVisible(true);
+                fr.setVisible(true);
+                //frame.setVisible(true);
                 //frame2.setVisible(true);
                 //histogramTest.setVisible(true);
             }
